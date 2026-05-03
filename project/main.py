170af5e1a6807d47310db1dd4179e5932e4583aa
kirill <kirillqwerttt@gmail.com>
2026-05-03 20:33:20 +0300

    Clone project

---
+++ project/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.routers import users, admin, words
+
+app = FastAPI(title="WordTracker API")
+
+app.include_router(users.router)
+app.include_router(admin.router)
+app.include_router(words.router)
+
+@app.get("/")
+async def home():
+    return {"message": "Hello, FastAPI!"}
